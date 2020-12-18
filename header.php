e488e00e4518759dfdde762a582876ac3c76461d
Sean Morrissey <morroclearwater@hotmail.com>
2020-12-18 16:11:08 +0800

    Added viewport header

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="<?php bloginfo( 'charset' )?>">
         <meta name="description" content="<?php bloginfo('description');?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>
             <?php if(is_front_page()):
                 bloginfo('name');
